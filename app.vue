d1c48726c14f395a0795dea320312189b3b81cbe
rizzra <rizzra26@gmail.com>
2023-09-27 09:39:33 +0700

    initialize project

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Welcome to my resume!</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: ref("Rizki R. | Resume"),
+  meta: [
+    {
+      name: "description",
+      content: "Rizki's Resume",
+    },
+  ],
+});
+</script>
